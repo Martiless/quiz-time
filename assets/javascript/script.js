dd48ab56318cc70f8bf2f62759dbd56edb303b0e
Martina Martin <marti.law@gmail.com>
2021-11-11 20:10:25 +0000

    Add import JSON to JavaScript file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,52 +1,76 @@
-// Wait for the DOM to finish loading before game runs. 
-// Code used in this project is influneced by code used in Code Institutes Love Maths project.
+// Import data from JSON file
+fetch("assets/data/questions.json")
+    .then(res => res.json())
+    .the(data => {}
 
-document.addEventListener("DOMContentLoaded", function () {
+        fetch("assets/data/leaderboard.json")
+        .then(res => res.json())
+        .the(data => {}
 
-})
 
-function runGame() {
+            // Wait for the DOM to finish loading before game runs. 
+            // Code used in this project is influneced by code used in Code Institutes Love Maths project.
 
-}
+            document.addEventListener("DOMContentLoaded", function () {
+                    let buttons = document.getElementsByTagName("button");
 
-function checkAnswers() {
+                    for (let button of buttons) {
+                        button.addEventListener("click", function () {
+                                if (this.getAttribute("data-type") === "movies") {
+                                    alert("You Picked Movies!");
+                                } else {
+                                    (this.getAttribute("data-type") === "tv-show") {
+                                        alert("You Picked TV Shows!");
+                                    }
+                                });
+                        }
 
-}
+                    })
 
-function chooseQuestion() {
 
-}
 
-function displayMovieQuestions() {
+                function runGame() {
 
-}
+                }
 
-function displayTVQuestions() {
+                function checkAnswers() {
 
-}
+                }
 
-/**
- * Gets current correct score from the DOM nad increases it by 1
- * This code was taken from Code Institutes Love Maths Project
- */
-function correctAnswerScore() {
-    let score = parseInt(document.getElementById("correct").innerText);
-    document.getElementById("correct").innerText = ++score;
-}
+                function chooseQuestion() {
 
-/**
- * Gets current incorrect score from the DOM nad increases it by 1
- * This code was taken from Code Institutes Love Maths Project
- */
-function incorrectAnswerScore() {
-    let score = parseInt(document.getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innerText = ++score;
-}
+                }
 
-function finshGame() {
+                function displayMovieQuestions() {
 
-}
+                }
 
-function updateLeaderBoard() {
-    return playArray[Math.floor(Math.random()* playArray.lenght)]
-}
+                function displayTVQuestions() {
+
+                }
+
+                /**
+                 * Gets current correct score from the DOM nad increases it by 1
+                 * This code was taken from Code Institutes Love Maths Project
+                 */
+                function correctAnswerScore() {
+                    let score = parseInt(document.getElementById("correct").innerText);
+                    document.getElementById("correct").innerText = ++score;
+                }
+
+                /**
+                 * Gets current incorrect score from the DOM nad increases it by 1
+                 * This code was taken from Code Institutes Love Maths Project
+                 */
+                function incorrectAnswerScore() {
+                    let score = parseInt(document.getElementById("incorrect").innerText);
+                    document.getElementById("incorrect").innerText = ++score;
+                }
+
+                function finshGame() {
+
+                }
+
+                function updateLeaderBoard() {
+                    return playArray[Math.floor(Math.random() * playArray.lenght)]
+                }

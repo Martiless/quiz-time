c04d457b4039b27b8b46155d3460234bd0c4eee7
Martina Martin <marti.law@gmail.com>
2021-11-11 11:53:28 +0000

    Begin to add functions to script

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,52 +1,52 @@
 // Wait for the DOM to finish loading before game runs. 
 // Code used in this project is influneced by code used in Code Institutes Love Maths project.
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
 })
 
 function runGame() {
-    
+
 }
 
 function checkAnswers() {
-    
+
 }
 
 function chooseQuestion() {
-    
+
 }
 
 function displayMovieQuestions() {
-    
+
 }
 
 function displayTVQuestions() {
-    
+
 }
 
 /**
-* Gets current correct score from the DOM nad increases it by 1
-* This code was taken from Code Institutes Love Maths Project
-*/
+ * Gets current correct score from the DOM nad increases it by 1
+ * This code was taken from Code Institutes Love Maths Project
+ */
 function correctAnswerScore() {
     let score = parseInt(document.getElementById("correct").innerText);
     document.getElementById("correct").innerText = ++score;
 }
 
 /**
-* Gets current incorrect score from the DOM nad increases it by 1
-* This code was taken from Code Institutes Love Maths Project
-*/
+ * Gets current incorrect score from the DOM nad increases it by 1
+ * This code was taken from Code Institutes Love Maths Project
+ */
 function incorrectAnswerScore() {
     let score = parseInt(document.getElementById("incorrect").innerText);
     document.getElementById("incorrect").innerText = ++score;
 }
 
 function finshGame() {
-    
+
 }
 
-function leaderBoard() {
-    
+function updateLeaderBoard() {
+    return playArray[Math.floor(Math.random()* playArray.lenght)]
 }

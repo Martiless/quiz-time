2507d3d491f5913ecbf94b21efb6c8435cc25d0f
Martina Martin <marti.law@gmail.com>
2022-02-19 13:40:43 +0000

    Edit questions in questions.js file

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,101 +1,210 @@
 const questions = [{
-        question: "Who has selective mutism in The Big Bang Theory?",
-        answers: [{
-                text: 'Howard',
-                correct: false
-            },
-            {
-                text: 'Raj',
-                correct: true
-            },
-            {
-                text: 'Amy',
-                correct: false
-            },
-            {
-                text: 'Stuart',
-                correct: false
-            }
-        ]
+    question: "Who has selective mutism in The Big Bang Theory?",
+    answers: [{
+        text: 'Howard',
+        correct: false
     },
-
     {
-        question: "What is Dexter Morgan's profession in the show 'Dexter'",
-        answers: [{
-                text: "Homicide Detective",
-                correct: false
-            },
-            {
-                text: "Blood Spatter Analyst",
-                correct: true
-            },
-            {
-                text: "Morgue Technician",
-                correct: false
-            },
-            {
-                text: "Barista",
-                correct: false
-            }
-        ]
+        text: 'Raj',
+        correct: true
     },
-
     {
-        question: "In 'Master of None' where does Dev move to after being dropped from a big sci-fi movie?",
-        answers: ["France", "Spain", "Greece", "Italy"],
-        correctAnswer: "Italy"
-
+        text: 'Amy',
+        correct: false
     },
-
-
-
     {
-        question: "In what U.S. state is Breaking Bad set?",
-        answers: ["New Mexico", "Virginia", "Arizona", "Oregon"],
-        correctAnswer: "New Mexico"
-
-    },
-
-    {
-        question: "Scrubs actor Zach Braff directed an episode of which award winning TV show about soccor?",
-        answers: ["Ted Lasso", "The Kicks", "All or Nothing", "Club of Crows"],
-        correctAnswer: "Ted Lasso"
-
-    },
-
-    {
-        question: "What’s the name of the skyscraper in Die Hard?",
-        answers: ["Nakatomi Plaza", "Nakatomi Tower", "Nakatomi Building", "Nakatomi Place"],
-        correctAnswer: "Nakatomi Plaza"
-    },
-
-
-    {
-        Question: "Which award winning director plays Korg in Thor: Ragnarok",
-        answers: ["Tim Burton", "J. J. Abrams", "Taika Waititi", "Anthony Rosso"],
-        correctAnswer: "Taika Waititi"
-
-    },
-
-
-    {
-        question: "Which Harry Potter star will be playing the dark knight in Matt Reeves upcoming movie 'The Batman'?",
-        answers: ["Tom Felton", "Daniel Radcliffe", "Matthew Lewis", "Robert Pattinson"],
-        correctAnswer: "Robert Pattinson"
-
-    },
-
-    {
-        question: "What is the highest-grossing movie of all time (not adjusted for inflation)?",
-        answers: ["Titanic", "Avatar", "Avengers Endgame", "Star Wars V"],
-        correctAnswer: "Avengers Endgame"
-
-    },
-
-    {
-        question: "Which one of these Tim Burton films did composer Danny Elfman voice a character?",
-        answers: ["Bettle Juice", "Nightmare Before Christmas", "Planet of the Apes", "Frankenweenie"],
-        correctAnswer: "Nightmare Before Christmas"
-
+        text: 'Stuart',
+        correct: false
     }
+    ],
+},
+
+{
+    question: "What is Dexter Morgan's profession in the show 'Dexter'",
+    answers: [{
+        text: "Homicide Detective",
+        correct: false
+    },
+    {
+        text: "Blood Spatter Analyst",
+        correct: true
+    },
+    {
+        text: "Morgue Technician",
+        correct: false
+    },
+    {
+        text: "Barista",
+        correct: false
+    }
+    ],
+},
+
+{
+    question: "In 'Master of None' where does Dev move to after being dropped from a big sci-fi movie?",
+    answers: [{
+        text: "France",
+        correct: false
+    },
+    {
+        text: "Spain",
+        correct: false
+    },
+    {
+        text: "Greece",
+        correct: false
+    },
+    {
+        text: "Italy",
+        correct: true
+    }
+    ],
+},
+
+{
+    question: "In what U.S. state is Breaking Bad set?",
+    answers: [{
+        text: "New Mexico",
+        correct: true
+    },
+    {
+        text: "Virginia",
+        correct: false
+    },
+    {
+        text: "Arizona",
+        correct: false
+    },
+    {
+        text: "Oregon",
+        correct: false
+    }
+    ],
+},
+
+{
+    question: "Scrubs actor Zach Braff directed an episode of which award winning TV show about soccor?",
+    answers: [{
+        text: "Ted Lasso",
+        correct: true
+    },
+    {
+        text: "The Kicks",
+        correct: false
+    },
+    {
+        text: "All or Nothing",
+        correct: false
+    },
+    {
+        text: "Club of Crows",
+        correct: false
+    }
+    ],
+},
+
+{
+    question: "What’s the name of the skyscraper in Die Hard?",
+    answers: [{
+        text: "Nakatomi Plaza",
+        correct: true
+    },
+    {
+        text: "Nakatomi Tower",
+        correct: false
+    },
+    {
+        text: "Nakatomi Building",
+        correct: false
+    },
+    {
+        text: "Nakatomi Place",
+        correct: false
+    }
+    ],
+},
+
+{
+    question: "Which award winning director plays Korg in Thor: Ragnarok",
+    answers: [{
+        text: "Tim Burton",
+        correct: false
+    },
+    {
+        text: "J.J. Abrams",
+        correct: false
+    },
+    {
+        text: "Taika Waititi",
+        correct: true
+    },
+    {
+        text: "Anthony Rosso",
+        correct: false
+    }
+    ],
+},
+
+{
+    question: "Which Harry Potter star will be playing the dark knight in Matt Reeves upcoming movie 'The Batman'?",
+    answers: [{
+        text: "Tom Felton",
+        correct: false
+    },
+    {
+        text: "Daniel Radcliffe",
+        correct: false
+    },
+    {
+        text: "Matthew Lewis",
+        correct: false
+    },
+    {
+        text: "Robert Pattinson",
+        correct: true
+    }
+    ],
+},
+
+{
+    question: "What is the highest-grossing movie of all time (not adjusted for inflation)?",
+    answers: [{
+        text: "Titanic",
+        correct: false
+    },
+    {
+        text: "Avatar",
+        correct: false
+    },
+    {
+        text: "Avengers Endgame",
+        correct: true
+    },
+    {
+        text: "Star Wars V",
+        correct: false
+    }
+    ],
+},
+
+{
+    question: "Which one of these Tim Burton films did composer Danny Elfman voice a character?",
+    answers: [{
+        text: "Bettle Juice",
+        correct: false
+    },
+    {
+        text: "Nightmare Before Christmas",
+        correct: true
+    },
+    {
+        text: "Planet of the Apes",
+        correct: false
+    },
+    {
+        text: "Frankenweenie",
+        correct: false
+    }
+    ],
+}
 ]

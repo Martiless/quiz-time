0033ac9aa598fb2ec99fd25cf5f6cdf7a6b7bb24
Martina Martin <marti.law@gmail.com>
2022-02-19 12:17:10 +0000

    Add resetUserScore, checkAnswer and clearColour function to JS file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -37,7 +37,7 @@ submitButtonElement.addEventListener("click", () => {
     alert("Thank you for submitting your username. We hope you enjoyed our game. Please press 'Restart Game' to play again 😎")
 })
 
-/* This function will run once the user clicks on the start button*/
+/** This function will run once the user clicks on the start button*/
 function startGame() {
     console.log('StartGame');
     startButton.classList.add('hidden');
@@ -49,19 +49,19 @@ function startGame() {
     callQuestions();
 }
 
-/* Once the user has started the game this function will initiate the questions */
+/** Once the user has started the game this function will initiate the questions */
 function callQuestions() {
     resetScreen;
     loadQuestions(shuffledQuestions[currentQuestionIndex]);
 }
 
-/*Calls the next question once the next button is pressed*/
+/**Calls the next question once the next button is pressed*/
 function nextQuestion() {
     resetScreen;
     loadQuestions(shuffledQuestions[currentQuestionIndex]);
 }
 
-/* Creates answer buttons in the HTML to house the answer variables in the questions object below*/
+/** Creates answer buttons in the HTML to house the answer variables in the questions object below*/
 function loadQuestions(questions) {
     scorePanelElement.classList.remove('hidden');
     nextButtonElement.classList.remove('hidden');
@@ -78,7 +78,7 @@ function loadQuestions(questions) {
     })
 }
 
-/* Clears the screen of the previous answer button text */
+/** Clears the screen of the previous answer button text */
 function resetScreen() {
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild);
@@ -86,7 +86,7 @@ function resetScreen() {
 }
 
 
-/* Checks the answer that has been selected, calls the userScore function & the answerChoice function */
+/** Checks the answer that has been selected, calls the userScore function & the answerChoice function */
 
 function checkAnswers(e) {
     selectedAnswer = e.target;
@@ -109,54 +109,33 @@ function checkAnswers(e) {
     answerChoice;
 }
 
-/* Increase the users score if the answer us correct */
+/** Increase the users score if the answer us correct */
 function userScore() {
     scoreCounter++;
     scoreCounterElemet.innerText = ++score;
 }
 
-/* May not be needed if I can put it into the selectAnswers function*/
-function checkAnswers(element, correct) {
-    resetPage
-    if (correct) {
-        element.classList.add('correct-btn');
-    } else {
-        element.classList.add('incorrect-btn');
-    }
+/** Resets the user score to 0 when the restart game button is pressed*/
+function resetUserScor() {
+    score = 0;
+    scoreCounterElemet.innerText = 0; 
 }
 
 
-function resetPage(element) {
+/** Changes the colour of the answer buttons to relfex the users correct or incorrect answer*/
+function checkAnswers(element, correct) {
+   clearColours(element);
+   if (correct === "true") {
+       element.classList.add('correct-btn');
+      } else {
+          element.classList.add('incorrect-btn');
+      }
+}
+
+/** Resets the colours of the buttons to the default colours when a new questions is loaded */
+function clearColours(element) {
     element.classList.remove('correct-btn')
     element.classList.remove('incorrect-btn')
 
 }
 
-
-/**
- * Gets current correct score from the DOM nad increases it by 1
- * This code was taken from Code Institutes Love Maths Project
- * *WILL EDIT TO FIT GAME LATER
- */
-function correctAnswerScore() {
-    let score = parseInt(document.getElementById("correct").innerText);
-    document.getElementById("correct").innerText = ++score;
-}
-
-/**
- * Gets current incorrect score from the DOM nad increases it by 1
- * This code was taken from Code Institutes Love Maths Project
- * *WILL EDIT TO FIT GAME LATER
- */
-function incorrectAnswerScore() {
-    let score = parseInt(document.getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innerText = ++score;
-}
-
-/* This function will be called at the end of the game. It will show the users score and also ask them to enter a username*/
-function finshGame() {
-    questionPanelElement.classList.add('hidden');
-    scorePanelElement.classList.add('hidden');
-    usernameElement.classList.remove('hidden');
-
-}
